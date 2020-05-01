e35734894e8f7f97aae53077bbfbe046f883643e
alfaried <alfaried@hotmail.com>
2020-05-01 19:39:40 +0800

    initialized style

--- flying-cat/src/main.js
+++ flying-cat/src/main.js
@@ -1,8 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+import 'normalize.css/normalize.css'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+
+// import '@/styles/variables.scss'
+// import '@/styles/index.scss'
+import '@/icons'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
