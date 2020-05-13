23b9a7d5cbee2b9365a5dea517d9ec751263a315
alfaried <alfaried@hotmail.com>
2020-05-13 12:06:42 +0800

    Shift the indexing for course data

--- cloudtopus_frontend/src/views/course/courseInfo.js
+++ cloudtopus_frontend/src/views/course/courseInfo.js
@@ -1,51 +1,53 @@
-const courseList = [{
-  label: 'IS101',
-  title: 'IS101 - Thursday, 12:00 - 15:15',
-  courseCode: 'IS101',
-  courseName: 'Introduction to Information Systems',
-  venue: 'SIS Seminar Rm 3-5',
-  classes: [{
-    section: 'G1',
-    teamPax: '8',
-    studentPax: '40'
+const courseList = [
+  {
+    label: 'ESM240',
+    title: 'ESM240 - Tuesday, 12:00 - 15:15',
+    courseCode: 'ESM240',
+    courseName: 'Enterprise Solution Management',
+    venue: 'SIS Seminar Rm 2-3',
+    classes: [{
+      section: 'G1',
+      teamPax: '7',
+      studentPax: '35'
+    }, {
+      section: 'G2',
+      teamPax: '6',
+      studentPax: '42'
+    }, {
+      section: 'G3',
+      teamPax: '6',
+      studentPax: '43'
+    }],
+    courseDesc: 'blah blah blah'
   }, {
-    section: 'G2',
-    teamPax: '9',
-    studentPax: '45'
-  }],
-  courseDesc: 'blah blah blah'
-}, {
-  label: 'ESM240',
-  title: 'ESM240 - Tuesday, 12:00 - 15:15',
-  courseCode: 'ESM240',
-  courseName: 'Enterprise Solution Management',
-  venue: 'SIS Seminar Rm 2-3',
-  classes: [{
-    section: 'G1',
-    teamPax: '7',
-    studentPax: '35'
+    label: 'IS101',
+    title: 'IS101 - Thursday, 12:00 - 15:15',
+    courseCode: 'IS101',
+    courseName: 'Introduction to Information Systems',
+    venue: 'SIS Seminar Rm 3-5',
+    classes: [{
+      section: 'G1',
+      teamPax: '8',
+      studentPax: '40'
+    }, {
+      section: 'G2',
+      teamPax: '9',
+      studentPax: '45'
+    }],
+    courseDesc: 'blah blah blah'
   }, {
-    section: 'G2',
-    teamPax: '6',
-    studentPax: '42'
-  }, {
-    section: 'G3',
-    teamPax: '6',
-    studentPax: '43'
-  }],
-  courseDesc: 'blah blah blah'
-}, {
-  label: 'MCRA380',
-  title: 'MCRA380 - Wednesday, 15:30 - 18:45',
-  courseCode: 'MCRA380',
-  courseName: 'Managing Customer Relationship',
-  venue: 'SOE/SOSS Seminar Rm 3-3',
-  classes: [{
-    section: 'G1',
-    teamPax: '9',
-    studentPax: '48'
-  }],
-  courseDesc: 'blah blah blah'
-}]
+    label: 'MCRA380',
+    title: 'MCRA380 - Wednesday, 15:30 - 18:45',
+    courseCode: 'MCRA380',
+    courseName: 'Managing Customer Relationship with Analytics',
+    venue: 'SOE/SOSS Seminar Rm 3-3',
+    classes: [{
+      section: 'G1',
+      teamPax: '9',
+      studentPax: '48'
+    }],
+    courseDesc: 'blah blah blah'
+  }
+]
 
 export { courseList }
